a994d45a99b13241bfab44955c3f3541d46b72ad
Ajay M <ajay.m05@ad.infosys.com>
2020-10-26 17:02:50 +0530

    youtube

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,61 +1,80 @@
-import React,{ useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import './Movies.css'
 import './Selected.css'
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer'
 
-function Rows({title,url,originals}){
-    const [movies,setMovies]=useState([]);
-    const [selectedMovie,setselectedMovie]=useState([])
-    const [open,setOpen]=useState(false)
-    const baseUrl="https://api.themoviedb.org/3"
-    const imageUrl="https://image.tmdb.org/t/p/original"
-    useEffect(()=>{
-        async function fetchData(){
-            const request=await axios.get(baseUrl+url);
+function Rows({ title, url, originals }) {
+    const [movies, setMovies] = useState([]);
+    const [selectedMovie, setselectedMovie] = useState([])
+    const [open, setOpen] = useState(false)
+    const [trailerUrl, setTrailerUrl] = useState("")
+    const baseUrl = "https://api.themoviedb.org/3"
+    const imageUrl = "https://image.tmdb.org/t/p/original"
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(baseUrl + url);
             setMovies(request.data.results);
             return request;
         }
         fetchData()
-    },[url]);
+    }, [url]);
 
-    const handleClick=(movie)=>{
+    const handleClick = (movie) => {
+
         setselectedMovie(movie)
-        if(selectedMovie===movie){
+        if (selectedMovie === movie) {
             setOpen(false)
             setselectedMovie([])
-        }else setOpen(true)
+        } else {
+            setOpen(true)
+            movieTrailer(movie?.title || movie?.name || movie?.original_name)
+                .then(url => {
+                    const urlparams = new URLSearchParams(new URL(url).search)
+                    setTrailerUrl(urlparams.get('v'))
+                }).catch(error => console.log(error))
+        }
     }
-    
-  return(
-        <div className="row">
-            <span className="title">{title}</span>
-            <div className="row-posters">
-                {movies.map((movie)=>{
-                    return(
-                        <img onClick={()=>handleClick(movie)} className={`poster ${originals && 'poster-originals'}`} key={movie.id} src={`${imageUrl}${originals?movie.poster_path:movie.backdrop_path}`} alt={movie.name}></img>
-                    )
-                })}
-            </div>
-            {open?
-            
-            <div className="selectedMovie">
-                <header className="selectedMovie-image" style={{backgroundImage:`url(${imageUrl}${selectedMovie?.backdrop_path})`,backgroundSize:"cover",backgroundPosition:"right"}}>
-            <div className="selectedMovie-content">
-                <h1 className="selectedMovie-title">{selectedMovie?.title||selectedMovie?.name||selectedMovie?.original_name}({(selectedMovie?.first_air_date?.slice(0,4)||selectedMovie?.release_date.slice(0,4)) })</h1>
-                <span className="rating">{(selectedMovie?.vote_average) }<span>&#9734;</span></span>
-                <div className="selectedMovie-buttons">
-                    <button className="selectedMovie-button">Play</button>
-                    <button className="selectedMovie-button">My List</button>
+
+    const opts = {
+        height: "140",
+        width: "70%",
+        playerVars: {
+            autoPlay: 1,
+
+        }
+    }
+
+    return (
+        <div>
+
+            <div className="row">
+                <span className="title">{title}</span>
+                <div className="row-posters">
+                    {movies.map((movie) => {
+                        return (
+                            <img onClick={() => handleClick(movie)} className={`poster ${originals && 'poster-originals'}`} key={movie.id} src={`${imageUrl}${originals ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}></img>
+                        )
+                    })}
                 </div>
-                <h1 className="selectedMovie-description">{(selectedMovie?.overview) }</h1>
+                {open ?
+
+                    <div className="selectedMovie">
+                        <header className="selectedMovie-image" style={{ backgroundImage: `url(${imageUrl}${selectedMovie?.backdrop_path})`, backgroundSize: "cover", backgroundPosition: "right" }}>
+                            <div className="selectedMovie-content">
+                                <h1 className="selectedMovie-title">{selectedMovie?.title || selectedMovie?.name || selectedMovie?.original_name}({(selectedMovie?.first_air_date?.slice(0, 4) || selectedMovie?.release_date.slice(0, 4))})</h1>
+                                
+                        <div className="trailer"><YouTube videoId={trailerUrl} opts={opts}></YouTube></div>
+
+                            </div>
+                            
+                        </header>
+                    </div>
+                    : null
+                }
             </div>
-           
-        </header>
-            </div>
-            :null
-            }
         </div>
     )
-
 }
 export default Rows;
