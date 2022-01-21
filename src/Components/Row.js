72b936e8e76fec1a60c22bfe0d1f5a752de8330b
AjayRichi <65593822+AjayRichi@users.noreply.github.com>
2022-01-21 20:28:26 +0530

    Update Row.js

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -10,29 +10,51 @@ function Rows({ title, url, originals }) {
     const [selectedMovie, setselectedMovie] = useState([])
     const [open, setOpen] = useState(false)
     const [trailerUrl, setTrailerUrl] = useState("")
+    const [urlCheck, seturlCheck] = useState(false)
     const baseUrl = "https://api.themoviedb.org/3"
     const imageUrl = "https://image.tmdb.org/t/p/original"
+
     useEffect(() => {
         async function fetchData() {
-            const request = await axios.get(baseUrl + url);
-            setMovies(request.data.results);
-            return request;
+                await axios.get(baseUrl + url)
+                .then(
+                    request=>{
+                        setMovies(request.data.results);
+                        return request;
+                    }).catch(e=>{
+                        alert(e)
+                    })
         }
         fetchData()
     }, [url]);
+
+    const handlePlay =(selectedMovie)=>{
+        if(document.querySelector('#playbtn').textContent === "Play" ){
+            seturlCheck(true) 
+            document.querySelector('#playbtn').textContent="Close"      
+        }else{
+            setOpen(false)
+        }
+    }
 
     const handleClick = (movie) => {
         setselectedMovie(movie)
         if (selectedMovie === movie) {
             setOpen(false)
-            setselectedMovie([])
+            setselectedMovie([])      
         } else {
-            setOpen(true)
+            seturlCheck(false)
             movieTrailer(movie?.title || movie?.name || movie?.original_name)
                 .then(url => {
                     const urlparams = new URLSearchParams(new URL(url).search)
                     setTrailerUrl(urlparams.get('v'))
-                }).catch(setTrailerUrl(""))
+                    document.querySelector('#playbtn').textContent="Play"
+                }).catch(e=>{
+                    document.querySelector('#playbtn').textContent="Close"
+                    seturlCheck(false)
+                    setTrailerUrl("")
+                })
+            setOpen(true)
         }
     }
 
@@ -41,48 +63,40 @@ function Rows({ title, url, originals }) {
         width: "250",
         playerVars: {
             autoPlay: 1,
-
         }
     }
 
     return (
         <div>
-
             <div className="row">
                 <span className="title">{title}</span>
                 <div className="row-posters">
-                    {movies.map((movie) => {
+                    {movies.map((movie) => {  
                         return (
                             <img onClick={() => handleClick(movie)} className={`poster ${originals && 'poster-originals'}`} key={movie.id} src={`${imageUrl}${originals ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}></img>
                         )
                     })}
                 </div>
                 {open ?
-
                     <div className="selectedMovie">
                         <header className="selectedMovie-image" style={{ backgroundImage: `url(${imageUrl}${selectedMovie?.backdrop_path})`, backgroundSize: "cover", backgroundPosition: "right" }}>
                             <div className="selectedMovie-content">
-                                <div className="trailer">
-                                    {trailerUrl &&
-                                        <div>
-                                            <span className="trailer-title">{selectedMovie?.title || selectedMovie?.name || selectedMovie?.original_name}({(selectedMovie?.first_air_date?.slice(0, 4) || selectedMovie?.release_date.slice(0, 4))})</span>
+                                <div className='left'>
+                                <span className="selectedMovie-title">{selectedMovie?.title || selectedMovie?.name || selectedMovie?.original_name}({(selectedMovie?.first_air_date?.slice(0, 4) || selectedMovie?.release_date.slice(0, 4))})</span>
+                                    <div className="selectedMovie-buttons">
+                                        <button id='playbtn' className="selectedMovie-button" onClick={()=>handlePlay(selectedMovie)}>Play</button>
+                                        <button className="selectedMovie-button">My List</button>
+                                    </div>
+                                    <h1 className="selectedMovie-description">{(selectedMovie?.overview)}</h1>
+                                </div>
+                                <div>
+                                {urlCheck ? 
+                                        <div className='right'>
                                             <YouTube className="youtube" videoId={trailerUrl} opts={opts}></YouTube>
                                         </div>
-                                    }
+                                    : null}
                                 </div>
-                                    {
-                                        trailerUrl ? "" :
-                                            <div >
-                                                <span className="selectedMovie-title">{selectedMovie?.title || selectedMovie?.name || selectedMovie?.original_name}({(selectedMovie?.first_air_date?.slice(0, 4) || selectedMovie?.release_date.slice(0, 4))})</span>
-                                                <div className="selectedMovie-buttons">
-                                                    <button className="selectedMovie-button">Play</button>
-                                                    <button className="selectedMovie-button">My List</button>
-                                                </div>
-                                                <h1 className="selectedMovie-description">{(selectedMovie?.overview)}</h1>
-                                            </div>
-                                    }
                             </div>
-
                         </header>
                     </div>
                     : null
