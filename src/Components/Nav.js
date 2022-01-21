504e23fbf1a2b62cad32c6196ef1da1949fedd6a
AjayRichi <65593822+AjayRichi@users.noreply.github.com>
2022-01-21 20:28:03 +0530

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -10,20 +10,25 @@ function Nav() {
             }else handleShow(false)
         })
         return()=>{
-            window.removeEventListener("scroll")
+            window.removeEventListener("scroll",null)
         }
     },[])
+
+    const Play=()=>{
+        new Audio("./Netflix-Intro-Sound.mp3").play()
+    }
 
         return (
             <div className={`nav ${show && "nav_black"}`}>
                 <img
                     className="logo"
-                    src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
                     alt="Netflix-logo"
+                    onClick={()=>Play()}
                 />
                 <img
                     className="avatar"
-                    src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
                     alt="User-logo"
                 />
             </div>
