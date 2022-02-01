97838641ea282cde6fb3d3e27b2d19e93a08b9eb
AjayRichi <65593822+AjayRichi@users.noreply.github.com>
2022-02-01 21:03:06 +0530

    new

---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+import 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9w4UHOclBol94wKiY-Vm4OfU1weEVgpc",
+  authDomain: "netflix-clone-21fdf.firebaseapp.com",
+  projectId: "netflix-clone-21fdf",
+  storageBucket: "netflix-clone-21fdf.appspot.com",
+  messagingSenderId: "696052804310",
+  appId: "1:696052804310:web:f6806ff2023ef299b3b777"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth=getAuth(app)
+
+export {auth}
