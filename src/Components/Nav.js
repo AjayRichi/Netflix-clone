23b385be77af4391720aa84c6b471bd2e0814b00
AjayRichi <65593822+AjayRichi@users.noreply.github.com>
2022-02-01 21:07:33 +0530

    new

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,53 +1,52 @@
-import React, {  useEffect, useState  } from 'react';
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './Nav.css'
+import "./Nav.css";
 
 function Nav() {
-    const [show,handleShow]=useState(false)
-    const [logo,handleLogo]=useState("logo")
-    const navigate =useNavigate()
+  const [show, handleShow] = useState(false);
+  const [logo, handleLogo] = useState("logo");
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        window.addEventListener("scroll",()=>{
-            if(window.scrollY>100){
-                handleShow(true);
-            }else handleShow(false)
-        })
-        return()=>{
-            window.removeEventListener("scroll",null)
-        }
-    },[])
-    
-    const Play=()=>{
-        new Audio("./Netflix-Intro-Sound.mp3").play()
-        handleLogo("logo-animation")
-        setTimeout(changeHandle,3000)
-    }
+  useEffect(() => {
+    const unsubscribe=window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return unsubscribe;
+  }, []);
 
-    const changeHandle=()=>{
-        handleLogo("logo")
-    }
+  const Play = () => {
+    navigate("/")
+    new Audio("./Netflix-Intro-Sound.mp3").play();
+    handleLogo("logo-animation");
+    setTimeout(changeHandle, 3000);
+  };
 
-    const profile=()=>{
-        navigate("/signin")
-    }
+  const changeHandle = () => {
+    handleLogo("logo");
+  };
 
-        return (
-            <div className={`nav ${show && "nav_black"}`}>
-                <img
-                    id='play'
-                    className={logo}
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-                    alt="Netflix-logo"
-                    onClick={()=>Play()}
-                />
-                <img
-                    className="avatar"
-                    src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-                    alt="User-logo"
-                    onClick={()=>profile()}
-                />
-            </div>
-        )
+  const avatarHandle =()=>{
+    navigate("/profile")
+  }
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        id="play"
+        className={logo}
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+        alt="Netflix-logo"
+        onClick={() => Play()}
+      />
+      <img
+        className="avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="User-logo"
+        onClick={()=>avatarHandle()}
+      />
+    </div>
+  );
 }
 export default Nav;
