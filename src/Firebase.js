550971a4298c30af75e37ba0360e9234aa31cd5e
AjayRichi <65593822+AjayRichi@users.noreply.github.com>
2022-02-03 11:08:14 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,14 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9w4UHOclBol94wKiY-Vm4OfU1weEVgpc",
-  authDomain: "netflix-clone-21fdf.firebaseapp.com",
-  projectId: "netflix-clone-21fdf",
-  storageBucket: "netflix-clone-21fdf.appspot.com",
-  messagingSenderId: "696052804310",
-  appId: "1:696052804310:web:d88f8481ff218ba4b3b777"
+  apiKey: "AIzaSyCcHfZktKdpVBQ003kes3HqNP8EWz8tp-0",
+  authDomain: "netflix-clone-prod-2d755.firebaseapp.com",
+  projectId: "netflix-clone-prod-2d755",
+  storageBucket: "netflix-clone-prod-2d755.appspot.com",
+  messagingSenderId: "477714537376",
+  appId: "1:477714537376:web:d8f4a96e6b4a8f100d62c5"
 };
-
 const app = initializeApp(firebaseConfig);
 
 const auth=getAuth(app)
