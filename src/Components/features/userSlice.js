4831f342740653c331676aed34a0c68b4f205d8d
AjayRichi <65593822+AjayRichi@users.noreply.github.com>
2022-02-04 21:56:34 +0530

    new

--- src/Components/features/userSlice.js
+++ src/Components/features/userSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   user: null,
+  wishlist: [],
 };
 
 const userSlice = createSlice({
@@ -14,11 +15,26 @@ const userSlice = createSlice({
     logout: (state) => {
       state.user = null;
     },
+    wishlist: (state, action) => {
+      for (let i = 0; i <= state.wishlist.length; i++) {
+        if (state.wishlist[i]) {
+          if (state.wishlist[i].movie === action.payload.wishlist.movie) {
+            let arr = state.wishlist.filter(
+              (obj) => obj.movie !== action.payload.wishlist.movie
+            );
+            state.wishlist = arr;
+          }
+        }
+      }if(true){
+        state.wishlist.push(action.payload.wishlist);
+      }
+    },
   },
 });
 
-export const { login, logout } = userSlice.actions;
+export const { login, logout, wishlist } = userSlice.actions;
 
 export const selectUser = (state) => state.user.user;
+export const selectwishlist = (state) => state.user.wishlist;
 
 export default userSlice.reducer;
