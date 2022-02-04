4eb26f10bd3049c19d997c4fdb1af552b546bb04
AjayRichi <65593822+AjayRichi@users.noreply.github.com>
2022-02-04 21:55:13 +0530

    new

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,32 +1,45 @@
 import React from "react";
-import Nav from "./Nav";
 import "./Profile.css";
 import { auth } from "../Firebase";
 import { useSelector } from "react-redux";
-import { selectUser } from "./features/userSlice";
+import { selectUser, selectwishlist } from "./features/userSlice";
 import { useNavigate } from "react-router-dom";
 
 function Profile() {
   const user = useSelector(selectUser);
+  const wishlist = useSelector(selectwishlist);
   const navigate = useNavigate();
   const signout = () => {
     auth.signOut();
     navigate("/");
   };
+  const handleBack = () => {
+    window.history.back()
+  };
 
   return (
     <div className="profileScreen">
-      <Nav />
-      <div className="profileBody">
-        <h2>
-          Welcome to your Profile
-          <br/>
-          <span>{user.name}</span>
-        </h2>
-        <button className="signout" onClick={signout}>
+      <i className="fa fa-arrow-left" onClick={handleBack}></i><span className="wishlist">Profile & Wishlist</span>
+      <button className="signout" onClick={signout}>
           Sign Out
         </button>
+      <div className="profileBody">
+        <img
+          className="logo-avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="User-logo"
+        />
+        <h2 className="welcome">Welcome to your Profile</h2>
       </div>
+      <div>
+          {wishlist.map((movie) => {
+            return movie.done === "Added" ? (
+              ""
+            ) : (
+                <img className="profilePoster" src={`${movie.image}`} alt={movie.name}/>
+              );
+          })}
+        </div>
     </div>
   );
 }
